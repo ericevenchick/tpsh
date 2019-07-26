c07737f709e5e723181207bad8d5098222d9140c
Eric Evenchick <eric@evenchick.com>
2019-07-26 11:14:55 -0400

    added autocomplete and some scripting support

--- main.go
+++ main.go
@@ -4,112 +4,95 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/signal"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/c-bata/go-prompt"
+	"github.com/containous/yaegi/interp"
+	"github.com/containous/yaegi/stdlib"
 	"github.com/linklayer/go-socketcan/pkg/socketcan"
 )
 
-var Services = map[string]byte{
-	// KWP2000
-	"StartDiagnosticSession":                 0x10,
-	"ECUReset":                               0x11,
-	"ReadFreezeFrameData":                    0x12,
-	"ReadDiagnosticTroubleCodes":             0x13,
-	"ClearDiagnosticInformation":             0x14,
-	"ReadStatusOfDiagnosticTroubleCodes":     0x17,
-	"ReadDiagnosticTroubleCodesByStatus":     0x18,
-	"ReadECUIdentification":                  0x1a,
-	"StopDiagnosticSession":                  0x20,
-	"ReadDataByLocalIdentifier":              0x21,
-	"ReadDataByCommonIdentifier":             0x22,
-	"ReadMemoryByAddress":                    0x23,
-	"SetDataRates":                           0x26,
-	"SecurityAccess":                         0x27,
-	"DynamicallyDefineLocalIdentifier":       0x2c,
-	"WriteDataByCommonIdentifier":            0x2e,
-	"InputOutputControlByCommonIdentifier":   0x2f,
-	"InputOutputControlByLocalIdentifier":    0x30,
-	"StartRoutineByLocalIdentifier":          0x31,
-	"StopRoutineByLocalIdentifier":           0x32,
-	"RequestRoutineResultsByLocalIdentifier": 0x33,
-	"RequestDownload":                        0x34,
-	"RequestUpload":                          0x35,
-	"TransferData":                           0x36,
-	"RequestTransferExit":                    0x37,
-	"StartRoutineByAddress":                  0x38,
-	"StopRoutineByAddress":                   0x39,
-	"RequestRoutineResultsByAddress":         0x3a,
-	"WriteDataByLocalIdentifier":             0x3b,
-	"WriteMemoryByAddress":                   0x3d,
-	"TesterPresent":                          0x3e,
-	"EscCode":                                0x80,
-	// UDS Specific,
-	"CommunicationControl": 0x28,
+var Services = map[byte]string{
+	0x10: "StartDiagnosticSession",
+	0x11: "ECUReset",
+	0x12: "ReadFreezeFrameData",
+	0x13: "ReadDiagnosticTroubleCodes",
+	0x14: "ClearDiagnosticInformation",
+	0x17: "ReadStatusOfDiagnosticTroubleCodes",
+	0x18: "ReadDiagnosticTroubleCodesByStatus",
+	0x1a: "ReadECUIdentification",
+	0x20: "StopDiagnosticSession",
+	0x21: "ReadDataByLocalIdentifier",
+	0x22: "ReadDataByCommonIdentifier",
+	0x23: "ReadMemoryByAddress",
+	0x26: "SetDataRates",
+	0x27: "SecurityAccess",
+	0x2c: "DynamicallyDefineLocalIdentifier",
+	0x2e: "WriteDataByCommonIdentifier",
+	0x2f: "InputOutputControlByCommonIdentifier",
+	0x30: "InputOutputControlByLocalIdentifier",
+	0x31: "StartRoutineByLocalIdentifier",
+	0x32: "StopRoutineByLocalIdentifier",
+	0x33: "RequestRoutineResultsByLocalIdentifier",
+	0x34: "RequestDownload",
+	0x35: "RequestUpload",
+	0x36: "TransferData",
+	0x37: "RequestTransferExit",
+	0x38: "StartRoutineByAddress",
+	0x39: "StopRoutineByAddress",
+	0x3a: "RequestRoutineResultsByAddress",
+	0x3b: "WriteDataByLocalIdentifier",
+	0x3d: "WriteMemoryByAddress",
+	0x3e: "TesterPresent",
+	0x80: "EscCode",
+	0x28: "CommunicationControl",
 }
-var NegativeResponseCodes = map[string]byte{
-	"positiveResponse":                       0x00,
-	"generalReject":                          0x10,
-	"serviceNotSupported":                    0x11,
-	"subFunctionNotSupported":                0x12,
-	"incorrectMessageLengthOrInvalidFormat":  0x13,
-	"responseTooLong":                        0x14,
-	"busyRepeatRequest":                      0x21,
-	"conditionsNotCorrect":                   0x22,
-	"requestSequenceError":                   0x24,
-	"requestOutOfRange":                      0x31,
-	"securityAccessDenied":                   0x33,
-	"invalidKey":                             0x35,
-	"exceedNumberOfAttempts":                 0x36,
-	"requiredTimeDelayNotExpired":            0x37,
-	"uploadDownloadNotAccepted":              0x70,
-	"transferDataSuspended":                  0x71,
-	"generalProgrammingFailure":              0x72,
-	"wrongBlockSequenceCounter":              0x73,
-	"responsePending":                        0x78,
-	"subFunctionNotSupportedInActiveSession": 0x7E,
-	"serviceNotSupportedInActiveSession":     0x7F,
-	"rpmTooHigh":                             0x81,
-	"rpmTooLow":                              0x82,
-	"engineIsRunning":                        0x83,
-	"engineIsNotRunning":                     0x84,
-	"engineRunTimeTooLow":                    0x85,
-	"temperatureTooHigh":                     0x86,
-	"temperatureTooLow":                      0x87,
-	"vehicleSpeedTooHigh":                    0x88,
-	"vehicleSpeedTooLow":                     0x89,
-	"throttle/PedalTooHigh":                  0x8A,
-	"throttle/PedalTooLow":                   0x8B,
-	"transmissionRangeNotInNeutral":          0x8C,
-	"transmissionRangeNotInGear":             0x8D,
-	"brakeSwitchNotClosed":                   0x8F,
-	"shifterLeverNotInPark":                  0x90,
-	"torqueConverterClutchLocked":            0x91,
-	"voltageTooHigh":                         0x92,
-	"voltageTooLow":                          0x93,
-}
-
-func GetNrcName(sid byte) string {
-	for k, v := range NegativeResponseCodes {
-		if sid == v {
-			return k
-		}
-	}
-	return "UnknownNRC"
-	return fmt.Sprintf("UnknownNRC($%02X)", sid)
-}
-
-func GetServiceName(sid byte) string {
-	for k, v := range Services {
-		if sid == v {
-			return k
-		}
-	}
-	return fmt.Sprintf("UnknownService($%02X)", sid)
+var NegativeResponseCodes = map[byte]string{
+	0x00: "positiveResponse",
+	0x10: "generalReject",
+	0x11: "serviceNotSupported",
+	0x12: "subFunctionNotSupported",
+	0x13: "incorrectMessageLengthOrInvalidFormat",
+	0x14: "responseTooLong",
+	0x21: "busyRepeatRequest",
+	0x22: "conditionsNotCorrect",
+	0x24: "requestSequenceError",
+	0x31: "requestOutOfRange",
+	0x33: "securityAccessDenied",
+	0x35: "invalidKey",
+	0x36: "exceedNumberOfAttempts",
+	0x37: "requiredTimeDelayNotExpired",
+	0x70: "uploadDownloadNotAccepted",
+	0x71: "transferDataSuspended",
+	0x72: "generalProgrammingFailure",
+	0x73: "wrongBlockSequenceCounter",
+	0x78: "responsePending",
+	0x7E: "subFunctionNotSupportedInActiveSession",
+	0x7F: "serviceNotSupportedInActiveSession",
+	0x81: "rpmTooHigh",
+	0x82: "rpmTooLow",
+	0x83: "engineIsRunning",
+	0x84: "engineIsNotRunning",
+	0x85: "engineRunTimeTooLow",
+	0x86: "temperatureTooHigh",
+	0x87: "temperatureTooLow",
+	0x88: "vehicleSpeedTooHigh",
+	0x89: "vehicleSpeedTooLow",
+	0x8A: "throttle/PedalTooHigh",
+	0x8B: "throttle/PedalTooLow",
+	0x8C: "transmissionRangeNotInNeutral",
+	0x8D: "transmissionRangeNotInGear",
+	0x8F: "brakeSwitchNotClosed",
+	0x90: "shifterLeverNotInPark",
+	0x91: "torqueConverterClutchLocked",
+	0x92: "voltageTooHigh",
+	0x93: "voltageTooLow",
 }
 
 var isotp socketcan.Interface
@@ -118,9 +101,9 @@ var tp_stop chan struct{}
 
 func printResponse(resp []byte) {
 	if resp[0] == 0x7F {
-		fmt.Printf("NRC: %s (%X)\n", GetNrcName(resp[2]), resp[2])
+		fmt.Printf("NRC: %s (%X)\n", NegativeResponseCodes[resp[2]], resp[2])
 	} else {
-		fmt.Printf("Service: %s (%X)\n", GetServiceName(resp[0]-0x40), resp[0]-0x40)
+		fmt.Printf("Service: %s (%X)\n", Services[resp[0]-0x40], resp[0]-0x40)
 	}
 	fmt.Println(hex.Dump(resp))
 }
@@ -128,6 +111,7 @@ func printResponse(resp []byte) {
 func executor(t string) {
 	t = strings.ToLower(t)
 	t = strings.TrimSpace(t)
+	args := strings.Split(t, " ")
 
 	if t == "quit" || t == "q" {
 		os.Exit(0)
@@ -150,6 +134,25 @@ func executor(t string) {
 			}
 		}
 
+	}
+
+	if args[0] == "run" {
+		fmt.Println(args[1])
+		script, err := ioutil.ReadFile(args[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		i := interp.New(interp.Options{})
+		i.Use(stdlib.Symbols)
+		_, err = i.Eval(string(script))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		return
 	}
 
 	if t == "tp" {
@@ -207,7 +210,26 @@ func executor(t string) {
 }
 
 func completer(d prompt.Document) []prompt.Suggest {
-	return []prompt.Suggest{}
+	args := strings.Split(d.TextBeforeCursor(), " ")
+	s := []prompt.Suggest{}
+
+	if len(args) == 1 {
+		// sort service map to ensure order
+		var keys []int
+		for k := range Services {
+			keys = append(keys, int(k))
+		}
+		sort.Ints(keys)
+
+		for _, k := range keys {
+			s = append(s, prompt.Suggest{Text: fmt.Sprintf("%02X", k), Description: Services[byte(k)]})
+		}
+		s = append(s, prompt.Suggest{Text: "quit", Description: "Quit application"})
+		s = append(s, prompt.Suggest{Text: "monitor", Description: "Monitor for traffic"})
+		s = append(s, prompt.Suggest{Text: "tp", Description: "Toggle TesterPresent transmission"})
+		return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+	}
+	return s
 }
 
 func parseCanId(s string) (uint32, error) {
